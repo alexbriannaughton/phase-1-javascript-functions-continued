754712139f67991265434d15c42badc5aaa72365
Alex Brian Naughton <alexbriannaughton@gmail.com>
2022-09-19 19:16:02 -0700

    commit tree 5aff2af48ad5a9083f80895ca714a56f3ac79dde

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun (verb="roller-skate") {
+    return `This Saturday, I want to ${verb}!`
+}
+function mondayWork (verb='go to the office') {
+    return `This Monday, I will ${verb}.`
+}
+
+function wrapAdjective (flair = '*') {
+    return function (parameter = 'special') {
+        return(`You are ${flair}${parameter}${flair}!`)
+    }
+}
+const alex = wrapAdjective('%')('awesome')
